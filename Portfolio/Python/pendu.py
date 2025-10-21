78d4d9d21cf5b6b7bcbf77de1c05d3d56a164e43
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2025-10-21 12:24:51 +0200

    Update
    meilleure version du pendu

--- Portfolio/Python/pendu.py
+++ Portfolio/Python/pendu.py
@@ -3,122 +3,137 @@ import random
 import turtle
 print("choose your level : a) easy b) medium c) hard")
 level=str(input())
+#choisis un mot au hazard dans la liste "mot"
+mota=["temps","route","fleur","coeur", "porte", "kayak", "bisou","bombe","ecrin","jeune","archer"]
+motb=["peinture","octogone","moccassin"]
+motc=["superstition","accrobranche",  "reminiscence", "architecture","consentement","sinusoidales"]
+
 if level=="a":
-    #choisis un mot au hazard dans la liste "mot"
-    mot=["temps","route","fleur","coeur", "porte", "kayak", "bisou","bombe","ecrin","jeune"]
-    random_index = random.randint(0,len(mot)-1)
-    motchoisi=mot[random_index]
-    #print (mot[random_index])
-    #print (motchoisi)
-    #répartie les lettres de ce mot dans une liste "motsecret"
-    motsecret=[]
-    mottirret=['-','-','-','-','-']
-    i=0
-    for i in range (0,5):
-        motsecret.append(motchoisi[i])
-
-    #print(motsecret)
-    erreur=0
-    valide=0
-    lettreu = []
-
-    while(erreur<6 and valide<5):
-        l=1
-        print("Donnez une lettre.")
-        lettre=str(input())
-        if lettre not in lettreu :
-            for f in range(0,len(motsecret)):
-                n=1+f
-            
-                if lettre==motsecret[f] :
-                    print(motsecret[f],n, "eme lettre")
-                    mottirret[f]=lettre
-                    print (mottirret)
-                    valide=valide+1
-                    l=2
-        else :
-            print ("cette lettre a déjà été donnée")
-            l=2
-        
-        
-                
-        if l==1 :
-            print("cette lettre ne fait pas partie du mot")
-            erreur= erreur+1
-            if erreur==1 :
-                turtle.setup(700, 1000)
-                turtle.color("black")
-                turtle.up()
-                turtle.goto(-100,-400)
-                turtle.down()
-                turtle.goto(-150,-400)
-                turtle.goto(-125,-400)
-                turtle.goto(-125,25)
-                turtle.goto(0,25)
-                turtle.goto(0,0)
-                turtle.up()
-                turtle.goto(0,-100)
-                turtle.down()
-            if erreur==2:
-                    ##cercle de 200 pixels de rayon
-                turtle.circle(50)
-                turtle.up()
-            if erreur==3:
-                    ##cercle de 200 pixels de rayon
-                turtle.goto(25,-55)
-                turtle.down()
-                turtle.goto(5,-30)
-                turtle.up()
-                turtle.goto(5,-50)
-                turtle.down()
-                turtle.goto(25,-30)
-                turtle.up()
-                turtle.goto(-25,-55)
-                turtle.down()
-                turtle.goto(-5,-30)
-                turtle.up()
-                turtle.goto(-5,-50)
-                turtle.down()
-                turtle.goto(-25,-30)
-                turtle.up()
-            if erreur==4:
-                    ##bras
-                turtle.goto(0,-100)
-                turtle.down()
-                turtle.goto(-75,-175)
-                turtle.up()
-                turtle.goto(0,-100)
-                turtle.down()
-                turtle.goto(75,-175)
-                turtle.up()
-            if erreur==5:
-                ##corp
-                turtle.up()
-                turtle.goto(0,-100)
-                turtle.down()
-                turtle.goto(0,-250)
-                turtle.up()
-
-        if l==2 :
-            lettreu.append(lettre)
-        
-        
-    if valide==5 :
-        print("bravo le mot était", motchoisi)
-    
-    if erreur==6 : 
-        print ("pas de chance, le mot était", motchoisi)
-            ##jambes
-        turtle.goto(0,-250)
-        turtle.down()
-        turtle.goto(-75,-325)
-        turtle.up()
-        turtle.goto(0,-250)
-        turtle.down()
-        turtle.goto(75,-325)
-        turtle.up()
-        turtle.exitonclick()
+    random_index = random.randint(0,len(mota)-1)
+    motchoisi = mota[random_index]
 if level=="b":
+    random_index = random.randint(0,len(motb)-1)
+    motchoisi = motb[random_index]
+if level=="c":
+    random_index = random.randint(0,len(motc)-1)
+    motchoisi = motc[random_index]
+motsecret=[]
+mottirret=[]
+i=0
+for i in range (0,len(motchoisi)):
+    motsecret.append(motchoisi[i])
+i=0
+for i in range (len(motsecret)):
+    mottirret.append('-')
+print("mot ici", mottirret)
+
+erreur=0
+valide=0
+lettreu = []
+
+while(erreur<6 and valide<len(motsecret)):
+    l=1
+    print("Donnez une lettre.")
+    lettre=str(input())
+    if lettre not in lettreu :
+        for f in range(0,len(motsecret)):
+            n=1+f
+        
+            if lettre==motsecret[f] :
+                print(motsecret[f],n, "eme lettre")
+                mottirret[f]=lettre
+                print (mottirret)
+                valide=valide+1
+                l=2
+    else :
+        print ("cette lettre a déjà été donnée")
+        l=2
+           
+    if l==1 :
+        print("cette lettre ne fait pas partie du mot")
+        erreur= erreur+1
+        if erreur==1 :
+            turtle.setup(700, 1000)
+            turtle.color("black")
+            turtle.up()
+            turtle.goto(-100,-400)
+            turtle.down()
+            turtle.goto(-150,-400)
+            turtle.goto(-125,-400)
+            turtle.goto(-125,25)
+            turtle.goto(0,25)
+            turtle.goto(0,0)
+            turtle.up()
+            turtle.goto(0,-100)
+            turtle.down()
+        if erreur==2:
+                ##cercle de 200 pixels de rayon
+            turtle.circle(50)
+            turtle.up()
+        if erreur==3:
+                ##cercle de 200 pixels de rayon
+            turtle.goto(25,-55)
+            turtle.down()
+            turtle.goto(5,-30)
+            turtle.up()
+            turtle.goto(5,-50)
+            turtle.down()
+            turtle.goto(25,-30)
+            turtle.up()
+            turtle.goto(-25,-55)
+            turtle.down()
+            turtle.goto(-5,-30)
+            turtle.up()
+            turtle.goto(-5,-50)
+            turtle.down()
+            turtle.goto(-25,-30)
+            turtle.up()
+        if erreur==4:
+                ##bras
+            turtle.goto(0,-100)
+            turtle.down()
+            turtle.goto(-75,-175)
+            turtle.up()
+            turtle.goto(0,-100)
+            turtle.down()
+            turtle.goto(75,-175)
+            turtle.up()
+        if erreur==5:
+            ##corp
+            turtle.up()
+            turtle.goto(0,-100)
+            turtle.down()
+            turtle.goto(0,-250)
+            turtle.up()
+
+    if l==2 :
+        lettreu.append(lettre)
+    
+    
+if valide==len(motsecret) :
+    print("bravo le mot était", motchoisi)
+
+if erreur==6 : 
+    print ("pas de chance, le mot était", motchoisi)
+        ##jambes
+    turtle.goto(0,-250)
+    turtle.down()
+    turtle.goto(-75,-325)
+    turtle.up()
+    turtle.goto(0,-250)
+    turtle.down()
+    turtle.goto(75,-325)
+    turtle.up()
+    turtle.exitonclick()
+    
+
+
+
+
+
+    # il se passe ça quand on apprend, on sait faire les trucs beaucoup mieux, 
+    # donc tout ce qui reste ici est absoluement innutile et à été remplacé par une dizaine de ligne...
+    '''if level=="b":
     mot=["peinture","octogone","moccassin"]
     random_index = random.randint(0,len(mot)-1)
     motchoisi=mot[random_index]
@@ -341,4 +356,4 @@ if level=="c":
         turtle.goto(75,-325)
         turtle.up()
         turtle.exitonclick()
-    
+    '''
